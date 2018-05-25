0a2a214dc23911ac89cf347212b5cdde5cc9d54f
todx2 <todzone1@aol.com>
2018-05-25 11:15:29 -0500

    Add clonez

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'secure-reaches-94228.herokuapp.com'
+  host = 'young-lowlands-95679.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
